78308d63c0a933b40a519f3eb73e9a2fd69aac66
RocketMan35 <harhyom9189@gmail.com>
2025-04-27 16:59:27 +0900

    api env에서 가져오기 확인

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 from fastapi import FastAPI
 from httpx import AsyncClient
+
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+# 환경 변수에서 API 키 가져오기
+NOTION_PRIVATE_API_KEY = os.getenv("NOTION_PRIVATE_API_KEY")
+if not NOTION_PRIVATE_API_KEY:
+    raise ValueError("API_KEY not found in environment variables")
+else:
+    print("NOTION_PRIVATE_API_KEY found")
+
 app = FastAPI()
 
 
